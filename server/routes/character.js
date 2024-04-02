8966738e7deda36aa3365e00afcf3ea666b3be14
Brock Bena <bbena99@gmail.com>
2024-04-02 10:20:49 -0500

    Update to the server

--- server/routes/character.js
+++ server/routes/character.js
@@ -3,14 +3,20 @@ const { charList } = require('../data/constant');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/:nameNoSpace',(req,res,next)=>{
+  console.log("GET "+req.url+" was hit")
+  let char = undefined;
+  charList.forEach(c=>{
+    if(c.nameNoSpace===req.params.nameNoSpace){
+      char = c;
+    }
+  })
+  char
+    ?res.status(200).send(char)
+    :res.status(404).send("ERROR: Did not find character by the name='"+req.params.nameNoSpace+"'")
+})
 router.get('/',(req, res, next)=>{
   res.status(200).send(JSON.stringify(charList));
 });
-router.get('/:nameNoSpace',(req,res,next)=>{
-  charList.forEach(c=>{
-    if(c.nameNoSpace===req.params.nameNoSpace)res.status(200).send(c)
-  })
-  res.status(404).send("ERROR: Did not find character by the name='"+req.params.nameNoSpace+"'")
-})
 
 module.exports = router;
