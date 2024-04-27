16c22f7c8e441a54c656300ec246f9b962e37f39
Brock Bena <bbena99@gmail.com>
2024-04-26 23:20:17 -0500

    Added engrave fields

--- server/data/constant.js
+++ server/data/constant.js
@@ -5,6 +5,8 @@ const charList = [
     'element' : 0,
     'class' : 0,
     'sign' : 0,
+    'engraveStat':7,
+    'engrave':[5.6,8.4,11.2,14,16.8],
     'base_stats':{
       'Attack'  : 789,
       'Defense' : 514,
@@ -22,6 +24,8 @@ const charList = [
     'element' : 4,
     'class' : 1,
     'sign' : 11,
+    'engraveStat':4,
+    'engrave':[6,9,12,15,18],
     'base_stats':{
       'Attack'  : 830,
       'Defense' : 713,
@@ -39,6 +43,8 @@ const charList = [
     'element' : 3,
     'class' : 2,
     'sign' : 0,
+    'engraveStat':7,
+    'engrave':[4.2,6.3,8.4,10.5,12.6,14.7],
     'base_stats':{
       'Attack'  : 1029,
       'Defense' : 473,
