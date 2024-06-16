80b34ee04d95692f52f8afb23b754f89ba0b1d1a
Brock Bena <bbena99@gmail.com>
2024-06-15 21:56:13 -0500

    changed public to client

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));//change this later
+app.use(express.static(path.join(__dirname, 'client')));//change this later
 
 const api = '/api/v1'
 app.use(api+'/Characters', characterRouter);
